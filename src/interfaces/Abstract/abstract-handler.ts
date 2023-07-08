e82e8aec5b05eab93fc57ba689086a5b88fe33de
seixeiro <nathan.seixeiro@gmail.com>
2023-07-08 00:36:50 -0300

    reafactor methods

--- src/interfaces/Abstract/abstract-handler.ts
+++ src/interfaces/Abstract/abstract-handler.ts
@@ -1,6 +1,6 @@
 import { INewCommand } from '@/interfaces/index'
 
 export interface IHandler {
-  setNext: (handler: IHandler) => IHandler
-  handler: (handler: INewCommand) => void
+  setNext(handler: IHandler): IHandler
+  handler(handler: INewCommand): void
 }
