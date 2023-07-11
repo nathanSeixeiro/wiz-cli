43777852f1d4756ec7e6ea5b1de539eda724c16d
seixeiro <nathan.seixeiro@gmail.com>
2023-07-11 00:20:28 -0300

    generate dotenv

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -2,6 +2,7 @@ import { GluegunToolbox } from 'gluegun'
 
 import { TypescriptHandle } from '../handlers'
 import { INewCommand } from '../interfaces/New/new-command'
+import { DotEnvHandle } from '../handlers/dotenv'
 
 module.exports = {
   name: 'new',
@@ -15,12 +16,15 @@ module.exports = {
     const name = parameters.first
 
     const tsHandle = new TypescriptHandle(toolbox)
+    const envHandle = new DotEnvHandle(toolbox)
+
+    tsHandle.setNext(envHandle)
 
     const request: INewCommand = { name }
-
-    tsHandle.handle(request)
-
     const spinner = spin('Generating files and installing dependencies')
+
+    await tsHandle.handle(request)
+
     spinner.stop()
 
     success('Done! Generated your new project setup!!')
---
+++ src/handlers/dotenv/index.ts
@@ -0,0 +1,25 @@
+import { GluegunToolbox } from 'gluegun'
+import { AbstractHandle } from '../abstract'
+import { INewCommand } from '../../interfaces/New/new-command'
+
+export class DotEnvHandle extends AbstractHandle {
+  constructor(private toolbox: GluegunToolbox) {
+    super()
+  }
+
+  public async handle(request: INewCommand) {
+    const { name } = request
+
+    await this.toolbox.template.generate({
+      template: 'env/.env.ejs',
+      target: `${name}/.env`,
+    })
+
+    await this.toolbox.template.generate({
+      template: 'env/.env.example.ejs',
+      target: `${name}/.env.example`,
+    })
+
+    return super.handle(request)
+  }
+}
