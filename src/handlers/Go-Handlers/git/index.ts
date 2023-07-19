10f2c0665193ddd57344de9d57c463df1eea6ac7
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-07-19 17:02:35 +0000

    add git

---
+++ src/handlers/Go-Handlers/git/index.ts
@@ -0,0 +1,26 @@
+import { GluegunToolbox } from 'gluegun'
+import { AbstractHandle } from '../../abstract'
+import { INewCommand } from '../../../interfaces'
+
+export class GitHandle extends AbstractHandle {
+  constructor(private toolbox: GluegunToolbox) {
+    super()
+  }
+
+  public async handle(request: INewCommand) {
+    const { name, initializeGitRepo } = request
+
+    if (initializeGitRepo) {
+      await this.toolbox.template.generate({
+        template: 'Go-Templates/git/.gitignore.ejs',
+        target: `${name}/.gitignore`,
+      })
+
+      await this.toolbox.system.run(
+        `cd ${name} && git init && git add . && git commit -m "initial commit"`
+      )
+
+      return super.handle(request)
+    }
+  }
+}
