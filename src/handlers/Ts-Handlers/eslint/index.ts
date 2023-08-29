38e69c863544de9fd404610284c01fa0cf384ab4
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-08-29 18:36:22 +0000

    feat: add command and configs for eslint

--- src/handlers/Ts-Handlers/eslint/index.ts
+++ src/handlers/Ts-Handlers/eslint/index.ts
@@ -1,28 +1,28 @@
-import { GluegunToolbox } from "gluegun";
-import { AbstractHandle } from "../../abstract";
-import { ITsCommand } from "../../../interfaces";
+import { GluegunToolbox } from 'gluegun'
+import { AbstractHandle } from '../../abstract'
+import { ITsCommand } from '../../../interfaces'
 
 export class EslintHandle extends AbstractHandle {
-    constructor(private toolbox: GluegunToolbox){
-        super()
+  constructor(private toolbox: GluegunToolbox) {
+    super()
+  }
+
+  public async handle(request: ITsCommand): Promise<void> {
+    const { name, eslint } = request
+
+    if (eslint) {
+      await this.toolbox.template.generate({
+        template: 'Ts-Templates/eslint/.eslintignore.ejs',
+        target: `${name}/.eslintinore`,
+      })
+
+      await this.toolbox.template.generate({
+        template: 'Ts-Templates/eslint/.eslintrc.js.ejs',
+        target: `${name}/.eslintrc.js`,
+      })
+
+      return super.handle(request)
     }
-
-    public async handle(request: ITsCommand):Promise<void> {
-        const {name, eslint} = request
-
-        if(eslint){
-            await this.toolbox.template.generate({
-                template: 'Ts-Templates/eslint/.eslintignore.ejs',
-                target: `${name}/.eslintinore`
-            })
-
-            await this.toolbox.template.generate({
-                template: 'Ts-Templates/eslint/.eslintrc.js.ejs',
-                target: `${name}/.eslintrc.js`
-            })
-
-            return super.handle(request)
-        }
-        return super.handle(request)
-    }
+    return super.handle(request)
+  }
 }
