a59e2d7bb0eef9021e2e2a1839d158e87936a536
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-10-20 18:04:11 +0000

    feat: add js command

---
+++ src/commands/new-js.ts
@@ -0,0 +1,46 @@
+import { GluegunCommand, GluegunToolbox } from 'gluegun'
+
+import { IJsCommand } from '../interfaces'
+import { JavascriptHandle } from '../handlers/Js-Handlers'
+
+const command: GluegunCommand = {
+  name: 'new-js',
+  alias: ['js'],
+  run: async (toolbox: GluegunToolbox) => {
+    const {
+      parameters,
+      requiredName,
+      print: { info, success },
+      prompt: { ask },
+    } = toolbox
+
+    const name = parameters.first
+    if (!name) {
+      requiredName(name)
+      return
+    }
+
+    const jsHandler = new JavascriptHandle(toolbox)
+
+    const askInitializeGitRepo = {
+      type: 'confirm',
+      name: 'initializeGitRepo',
+      message: 'Do you want to initialize a Git repository?',
+    }
+
+    const options = [askInitializeGitRepo]
+    const { initializeGitRepo } = await ask(options)
+
+    const request: IJsCommand = { name, initializeGitRepo }
+    jsHandler.handle(request)
+
+    info(`
+      Next:
+      $ cd ${name}
+      $ npm run dev
+      `)
+    success('Done! Generated your new project setup!!')
+  },
+}
+
+module.exports = command
